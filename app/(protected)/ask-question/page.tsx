913ae9e36c6e2043e4ebf2e7d6fc56bab9cc8a60
JaviPSanchez <javier.palomino-sanchez@outlook.com>
2024-05-16 22:15:33 +0200

    Ask-question component

---
+++ app/(protected)/ask-question/page.tsx
@@ -0,0 +1,14 @@
+import Question from "@/components/forms/question";
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
